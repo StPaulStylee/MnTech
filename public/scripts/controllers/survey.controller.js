62c0439d4b0fc6181ba9788d91de1c205ce55f5c
laserlens <laserlens@gmail.com>
2017-11-27 19:22:01 -0600

    removed sortable add ng-sortable

--- public/scripts/controllers/survey.controller.js
+++ public/scripts/controllers/survey.controller.js
@@ -4,7 +4,19 @@ angular.module('mnTechSite')
 function SurveyController(EmailService) {
   console.log('SurveyController Loaded');
 
-  var ctrl = this;
+  let ctrl = this;
+
+  ctrl.visiterList = [
+    'Resident - Looking for programs',
+    'Resident - Looking for engagement opportunities',
+    'Resident - Looking for events/calendar',
+    'Resident - Looking for information (history, minutes, local business, etc.)',
+    'Business - Looking for programs',
+    'Business - Looking for engagement opportunities',
+    'Business - Looking for events/calendar',
+    'Business - Looking for information (history, local business, etc.)',
+    'Funder - Looking for information about your organization'
+  ];
 
 //funtion sends form data to email service
   ctrl.sendSurveyData = function(data) {
